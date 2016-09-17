59ab819feac70a63a285492c8ee8581cedc53050
stanchin <ess.chin@gmail.com>
2016-09-17 18:33:20 -0400

    added back config

---
+++ config.py
@@ -0,0 +1,7 @@
+# Copy as "config.py" and insert your Yelp credentials. See README.md for details.
+
+yelp_api_auth = {
+    'client_id' : 'rQxgTpLT_OsY3nvdrcowIg',
+    'client_secret' : '59ah9NwqLRliPyrdVqkLafsOltdux37VcjfLQZ1BdyGT2mTVTP7q0MCTRXdJREcx',
+    'grant_type' : 'client_credentials'
+}
