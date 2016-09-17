6b7455702f39a17ddd98ffa7b9d2f8390d53d6fe
stanchin <ess.chin@gmail.com>
2016-09-17 13:47:21 -0400

    deleted app.py, replaced by __init__

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hi there, how ya doin?"
+	return render_template('main.html')
 
-
+port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=int(port))
